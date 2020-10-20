d2485d10279d37e652b309f6d4d27d39115cf2b1
m12watanabe1a <m12watanabe1a@gmail.com>
2020-10-20 22:01:36 +0900

    add color image to show

--- rs-imshow/rs-imshow.cpp
+++ rs-imshow/rs-imshow.cpp
@@ -4,23 +4,28 @@
 int main(int argc, char *argv[]) try {
   rs2::colorizer color_map;
   rs2::config cfg;
+  cfg.enable_stream(RS2_STREAM_COLOR, 848, 480, RS2_FORMAT_BGR8, 30);
   cfg.enable_stream(RS2_STREAM_DEPTH, 848, 480, RS2_FORMAT_Z16, 30);
 
   rs2::pipeline pipe;
   pipe.start(cfg);
-  const auto window_name = "Display Image";
-  cv::namedWindow(window_name, CV_WINDOW_AUTOSIZE);
+  cv::namedWindow("color", CV_WINDOW_AUTOSIZE);
+  cv::namedWindow("depth", CV_WINDOW_AUTOSIZE);
 
   while (true) {
     rs2::frameset data = pipe.wait_for_frames();
+    rs2::frame color = data.get_color_frame();
     rs2::frame depth = data.get_depth_frame().apply_filter(color_map);
     const int w = depth.as<rs2::video_frame>().get_width();
     const int h = depth.as<rs2::video_frame>().get_height();
     std::cout << "width: " << w << " height: " << h << std::endl;
 
-    cv::Mat image(cv::Size(w, h), CV_8UC3, (void *)depth.get_data(),
+    cv::Mat color_image(cv::Size(w, h), CV_8UC3, (void *)color.get_data(),
                   cv::Mat::AUTO_STEP);
-    cv::imshow(window_name, image);
+    cv::Mat depth_image(cv::Size(w, h), CV_8UC3, (void *)depth.get_data(),
+                  cv::Mat::AUTO_STEP);
+    cv::imshow("color", color_image);
+    cv::imshow("depth", depth_image);
     cv::waitKey(1);
   }
   return EXIT_SUCCESS;
